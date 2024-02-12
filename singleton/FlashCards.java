1b03ff0a8a47e15e1b60858bf3ef86dd27f061b2
jakeR-wq <jacobr10183@gmail.com>
2024-02-11 22:38:16 -0500

    adding javadoc

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,18 +1,32 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * The FlashCards class represents a set of flashcards containing words and their definitions.
+ * It allows users to retrieve a random word from the set.
+ */
 public class FlashCards {
     private Random rand;
     private static FlashCards flashCards;
     private ArrayList<Word> words;
     private Word currentWord;
 
+    /**
+     * Constructs a FlashCards object and initializes it with a list of words.
+     * The list of words is obtained from a file using the FileReader class.
+     * The FlashCards object also retrieves the first word from the list.
+     */
     public FlashCards() {
         this.rand = new Random();
         words = new ArrayList<Word>(FileReader.getWords());
         getWord();
     }
 
+    /**
+     * Returns the instance of FlashCards. If it doesn't exist, a new instance is created.
+     *
+     * @return The instance of FlashCards.
+     */
     public static FlashCards getInstance() {
         if (flashCards == null) {
             flashCards = new FlashCards();
@@ -20,6 +34,11 @@ public class FlashCards {
         return flashCards;
     }
 
+    /**
+     * retrieves a random word from the arrayList returned from filereader
+     * 
+     * @return the randomly selected word object
+     */
     public Word getWord() {
         this.currentWord = this.words.get(this.rand.nextInt(this.words.size()));
         return this.currentWord;
--- singleton/Word.java
+++ singleton/Word.java
@@ -1,9 +1,21 @@
+/**
+ * The Word class represents a word along with its type, definition, and an example sentence.
+ */
+
 public class Word {
     private String word;
     private String type;
     private String definition;
     private String sentence;
 
+    /**
+     * Constructs a Word object with the specified word, type, definition, and sentence.
+     *
+     * @param word The word itself.
+     * @param type The part of speech or type of the word.
+     * @param definition The definition of the word.
+     * @param sentence An example sentence demonstrating the usage of the word.
+     */
     public Word(String word, String type, String definition, String sentence) {
         this.word = word;
         this.type = type;
@@ -11,10 +23,20 @@ public class Word {
         this.sentence = sentence;
     }
 
+    /**
+     * Gets the word itself.
+     *
+     * @return The word.
+     */
     public String getWord() {
         return word;
     }
 
+    /**
+     * Constructs and returns a description of the word, including its type, definition, and an example sentence.
+     *
+     * @return A string containing the part of speech, definition, and example sentence of the word.
+     */
     public String getDescription() {
         StringBuilder sb = new StringBuilder();
         sb.append("Part of speech: ").append(this.type).append("\n")
